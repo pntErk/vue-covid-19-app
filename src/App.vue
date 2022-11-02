7c78d986d36995ba43659fd309c4286419f137c4
Erk <61740386+Erk-dev@users.noreply.github.com>
2022-11-02 16:00:25 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
 import Topwrapper from "./components/Topwrapper.vue";
 import CardCases from "./components/CardCases.vue";
 </script>
