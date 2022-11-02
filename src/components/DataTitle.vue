9c2e31047138de1e263f076de9cb67c55258fca0
Erk <61740386+Erk-dev@users.noreply.github.com>
2022-11-02 20:17:26 +0700

    Create DataTitle.vue

---
+++ src/components/DataTitle.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="text-center">
+    <h2 class="text-3xl font-bold">{{totalcases}}</h2>
+    <h5 class="TotalCases">Total Cases</h5>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DataTitle",
+  components: {},
+  props: ["totalcases"],
+};
+</script>
