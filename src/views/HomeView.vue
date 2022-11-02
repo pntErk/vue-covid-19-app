96a1adbb37355237c580e912d86711e40e17d3bb
Erk <61740386+Erk-dev@users.noreply.github.com>
2022-11-02 16:39:38 +0700

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,32 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">Hello World</div>
 </template>
+
+<script>
+export default {
+  name: "HomeView",
+  components: {},
+  data(){
+    return {
+      loading: true,
+      title: 'cases',
+      
+    }
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch("https://disease.sh/v3/covid-19/all");
+      const data = await res.json();
+      return data;
+    },
+  },
+  async created() {
+    const data = this.fetchCovidData()
+    console.log(data)
+  },
+};
+</script>
