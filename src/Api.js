0ef961a39a1a77fccc590d91ff40bbdb5806bc21
Erk <61740386+Erk-dev@users.noreply.github.com>
2022-11-02 18:31:48 +0700

    Create Api.js

---
+++ src/Api.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+
+const http = axios.create({
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://vue-covid-19-app.netlify.app"
+      : "http://127.0.0.1:5173/",
+});
+
+export default {
+  async getAllCases() {
+    // Get Overall Total Cases World Wide
+    const { data } = await http.get(
+      "https://disease.sh/v3/covid-19/all?yesterday=true&allowNull=true"
+    );
+    return data;
+  },
+  async getbyCountries() {
+    // Get Overall Total Cases in different Countries
+    const { data } = await http.get(
+      "https://disease.sh/v3/covid-19/countries?yesterday=true&sort=cases&allowNull=true"
+    );
+    return data;
+  },
+  async getByCountry(iso) {
+    // Get Overall Total Cases in a specific Countries by iso
+    const { data } = await http.get(
+      "https://disease.sh/v3/covid-19/countries/" +
+        iso +
+        "?yesterday=true&strict=true"
+    );
+    return data;
+  },
+  async getDailyCases() {
+    // Get Daily Overall Total Cases World Wide
+    const { data } = await http.get(
+      "https://disease.sh/v3/covid-19/historical/all?lastdays=30"
+    );
+    return data;
+  },
+  async getDailyCasesByCountry(iso) {
+    // Get Daily Cases in 30days on a specific Countries by iso
+    const { data } = await http.get(
+      "https://disease.sh/v3/covid-19/historical/" + iso + "?lastdays=30"
+    );
+    return data;
+  },
+};
