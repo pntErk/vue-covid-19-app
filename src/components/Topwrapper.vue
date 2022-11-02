7f50b9d09bd79e5e2539bcab1bdd2fbf689ef56c
Erk <61740386+Erk-dev@users.noreply.github.com>
2022-11-02 16:39:39 +0700

    Update Topwrapper.vue

--- src/components/Topwrapper.vue
+++ src/components/Topwrapper.vue
@@ -1,6 +1,7 @@
 <template>
-  <header>
-    <div>Covid-19 Tracker</div>
+  <header class="text-center bg-blue-800 text-white p-4 mb-10">
+    <div class="text-3xl md:text-5xl font-bold mb-3"><i class="fa fa-viruses"></i>Covid-19 Dashboard</div>
+    <p>API by <a class="text-blue-300" href="https://disease.sh/docs/">disease.sh</a></p>
   </header>
 </template>
 <script>
