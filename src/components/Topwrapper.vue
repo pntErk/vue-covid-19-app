49f3c551fd002f0121e281bd501af491cc452ba0
Erk <61740386+Erk-dev@users.noreply.github.com>
2022-11-02 21:14:40 +0700

    Update Topwrapper.vue

--- src/components/Topwrapper.vue
+++ src/components/Topwrapper.vue
@@ -1,8 +1,24 @@
 <template>
-  <header class="text-center bg-blue-800 text-white p-4 mb-10 flex">
-    <div class="text-3xl md:text-5xl font-bold mb-3"><i class="fa fa-viruses"></i>Covid-19 Dashboard</div>
-    <p>API by <a class="text-blue-300" href="https://disease.sh/docs/">disease.sh</a></p>
+  <header class="text-center bg-blue-800 text-white p-4 mb-10 rounded-md">
+    <div class="text-3xl md:text-5xl font-bold mb-3">
+      <i class="fa fa-viruses"></i>Covid-19 Dashboard
+    </div>
+    <p>
+      API by
+      <a class="text-blue-300" href="https://disease.sh/docs/">disease.sh</a>
+    </p>
   </header>
+  <!-- <div class="header-2">
+    <nav class="bg-white py-2 md:py-4">
+      <div class="container px-4 mx-auto md:flex md:items-center">
+        <div class="flex justify-between items-center">
+          <a href="#" class="font-bold text-xl text-indigo-600"
+            ><i class="fa fa-viruses"></i>Covid-19 Dashboard</a
+          >
+        </div>
+      </div>
+    </nav>
+  </div> -->
 </template>
 <script>
 export default {
