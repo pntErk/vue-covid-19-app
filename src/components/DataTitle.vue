5de786df49db850872069e777cb608a5397e511c
Erk <61740386+Erk-dev@users.noreply.github.com>
2022-11-03 15:23:14 +0700

    Update DataTitle.vue

--- src/components/DataTitle.vue
+++ src/components/DataTitle.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="text-center">
-    <!-- <h2 class="text-3xl font-bold">{{ totalcases }}</h2> -->
     <h2 class="text-3xl font-bold">
       <vue3-autocounter
         ref="counter"
@@ -11,18 +10,15 @@
       />
     </h2>
     <h5 class="TotalCases">Total Cases</h5>
-    <CardCases />
   </div>
 </template>
 
 <script>
-import CardCases from "./CardCases.vue";
 import Vue3autocounter from "vue3-autocounter";
 export default {
   name: "DataTitle",
   components: {
     "vue3-autocounter": Vue3autocounter,
-    CardCases
   },
   props: ["totalcases"],
 };
