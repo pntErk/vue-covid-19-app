61005d93c80f053e211b32c51c9898736ca0f57e
Erk <61740386+Erk-dev@users.noreply.github.com>
2022-11-03 15:08:11 +0700

    Update Topwrapper.vue

--- src/components/Topwrapper.vue
+++ src/components/Topwrapper.vue
@@ -1,6 +1,6 @@
 <template>
   <header
-    class="myheader bg-white-100 bg-slate-50 text-black p-4 mb-10 shadow-md sm:px-4"
+    class="myheader bg-white-100 bg-white rounded-lg border border-gray-200 text-black p-4 mb-10 shadow-md sm:px-4"
   >
   <div class="mb:flex mb:justify-between mb:items-center">
     <div class="text-3xl md:text-3xl font-bold mb-3">
@@ -12,17 +12,6 @@
     </p>
   </div>
   </header>
-  <!-- <div class="header-2">
-    <nav class="bg-white py-2 md:py-4">
-      <div class="container px-4 mx-auto md:flex md:items-center">
-        <div class="flex justify-between items-center">
-          <a href="#" class="font-bold text-xl text-indigo-600"
-            ><i class="fa fa-viruses"></i>Covid-19 Dashboard</a
-          >
-        </div>
-      </div>
-    </nav>
-  </div> -->
 </template>
 <script>
 export default {
